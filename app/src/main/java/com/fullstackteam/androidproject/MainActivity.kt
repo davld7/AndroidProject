a9967ec2e72fbb545350c04c5e57247d33af80e7
davld7 <118039588+davld7@users.noreply.github.com>
2023-03-23 19:12:49 -0600

    Agregué la variable binding.

--- app/src/main/java/com/fullstackteam/androidproject/MainActivity.kt
+++ app/src/main/java/com/fullstackteam/androidproject/MainActivity.kt
@@ -2,10 +2,13 @@ package com.fullstackteam.androidproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.fullstackteam.androidproject.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
