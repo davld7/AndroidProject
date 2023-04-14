ed9423f65aba9a14f95ad05dae33facd1c5b5e1e
Roberto David <118039588+davld7@users.noreply.github.com>
2023-04-13 20:14:49 -0600

    Agregué el botón para actualizar.

--- app/src/main/java/com/fullstackteam/androidproject/AnimeListActivity.kt
+++ app/src/main/java/com/fullstackteam/androidproject/AnimeListActivity.kt
@@ -7,7 +7,7 @@ import android.view.View
 import com.fullstackteam.androidproject.databinding.ActivityAnimeListBinding
 import com.fullstackteam.androidproject.model.AnimeDBClient
 import kotlin.concurrent.thread
-import android.util.Log
+import android.widget.Toast
 import com.fullstackteam.androidproject.model.AnimeAdapter
 
 class AnimeListActivity : AppCompatActivity() {
@@ -17,41 +17,49 @@ class AnimeListActivity : AppCompatActivity() {
         binding = ActivityAnimeListBinding.inflate(layoutInflater).apply {
             setContentView(root)
             buttonGoBack.text = "Regresar"
-        }
-        thread{
+            buttonUpdate.text = "Actualizar"
             loadAnimeList()
         }
     }
     fun goBack(view: View){
         finish()
     }
+    fun update(view: View){
+        loadAnimeList()
+    }
     private fun loadAnimeList(){
-        try {
-            val animeList = AnimeDBClient.service.animeList()
-            val response = animeList.execute()
-            val body = response.body()
+        thread {
+            try {
+                val animeList = AnimeDBClient.service.animeList()
+                val response = animeList.execute()
+                val animeListBody = response.body()
 
-            if (response.isSuccessful && body != null) {
-                runOnUiThread {
-                    val animeAdapter = AnimeAdapter(this, body)
-                    binding.listViewAnimeList.adapter = animeAdapter
-                }
-                binding.listViewAnimeList.setOnItemClickListener { parent, view, position, id ->
-                    val intent = Intent(AnimeListActivity@this, AnimeDetailActivity::class.java).apply{
-                        putExtra("name", body[id.toInt()].name)
-                        putExtra("description", body[id.toInt()].description)
-                        putExtra("episodes", body[id.toInt()].episodes)
-                        putExtra("season", body[id.toInt()].season)
-                        putExtra("genres", body[id.toInt()].genres)
-                        putExtra("image_url", body[id.toInt()].image_url)
+                if (response.isSuccessful && animeListBody != null) {
+                    runOnUiThread {
+                        val animeAdapter = AnimeAdapter(this, animeListBody)
+                        binding.listViewAnimeList.adapter = animeAdapter
                     }
-                    startActivity(intent)
+                    binding.listViewAnimeList.setOnItemClickListener { parent, view, position, id ->
+                        val intent = Intent(this, AnimeDetailActivity::class.java).apply{
+                            putExtra("name", animeListBody[position].name)
+                            putExtra("description", animeListBody[position].description)
+                            putExtra("episodes", animeListBody[position].episodes)
+                            putExtra("season", animeListBody[position].season)
+                            putExtra("genres", animeListBody[position].genres)
+                            putExtra("image_url", animeListBody[position].image_url)
+                        }
+                        startActivity(intent)
+                    }
+                } else {
+                    runOnUiThread {
+                        Toast.makeText(this, "La llamada a la API falló con el código de respuesta ${response.code()}", Toast.LENGTH_SHORT).show()
+                    }
                 }
-            } else {
-                Log.e("AnimeListActivity", "La llamada a la API falló con el código de respuesta ${response.code()}")
+            } catch (exception: Exception) {
+                runOnUiThread {
+                    Toast.makeText(this, "La llamada a la API falló con una excepción $exception", Toast.LENGTH_SHORT).show()
+                }
             }
-        } catch (e: Exception) {
-            Log.e("AnimeListActivity", "La llamada a la API falló con una excepción", e)
         }
     }
 }
