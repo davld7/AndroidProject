4ed05944d0751f427f68121b06a9713b02697b20
Roberto David <118039588+davld7@users.noreply.github.com>
2023-04-17 01:46:56 -0600

    Agregué comentarios.

--- app/src/main/java/com/fullstackteam/androidproject/MainActivity.kt
+++ app/src/main/java/com/fullstackteam/androidproject/MainActivity.kt
@@ -6,10 +6,11 @@ import android.os.Bundle
 import android.view.View
 import com.fullstackteam.androidproject.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {    
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // Inflamos el layout y configuramos las vistas
         binding = ActivityMainBinding.inflate(layoutInflater).apply {
             setContentView(root)
             textMainMenu.text = "Menú Principal"
@@ -17,6 +18,7 @@ class MainActivity : AppCompatActivity() {
             buttonAnimeList.text = "Lista de Animes"
         }
     }
+    // Función para ir a la pantalla de lista de animes
     fun goAnimeList(view: View) {
         val intent = Intent(this, AnimeListActivity::class.java)
         startActivity(intent)
