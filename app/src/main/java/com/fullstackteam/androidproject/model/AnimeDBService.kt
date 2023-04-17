7e97b0d1860cc12ce3f9a6c082dfb19140d2ef81
Roberto David <118039588+davld7@users.noreply.github.com>
2023-04-17 01:33:44 -0600

    Agregué comentarios.

--- app/src/main/java/com/fullstackteam/androidproject/model/AnimeDBService.kt
+++ app/src/main/java/com/fullstackteam/androidproject/model/AnimeDBService.kt
@@ -3,7 +3,10 @@ package com.fullstackteam.androidproject.model
 import retrofit2.Call
 import retrofit2.http.GET
 
+// Interfaz que define una llamada a un servicio web para obtener una lista de anime
 interface AnimeDBService{
+    // Anotación que indica que se va a realizar una petición GET a la URL "animes"
     @GET("animes")
+    // Función que se utiliza para obtener una lista de anime utilizando la biblioteca Retrofit
     fun animeList(): Call<AnimeList>
 }
