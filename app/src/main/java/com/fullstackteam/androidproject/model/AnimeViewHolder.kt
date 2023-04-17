09a7dbee8d8b1476dd487dc17f11733aa6731635
Roberto David <118039588+davld7@users.noreply.github.com>
2023-04-17 01:40:07 -0600

    Agregué comentarios.

--- app/src/main/java/com/fullstackteam/androidproject/model/AnimeViewHolder.kt
+++ app/src/main/java/com/fullstackteam/androidproject/model/AnimeViewHolder.kt
@@ -7,25 +7,32 @@ import androidx.recyclerview.widget.RecyclerView
 import com.fullstackteam.androidproject.databinding.AnimeItemBinding
 import com.squareup.picasso.Picasso
 
+// ViewHolder personalizado utilizado por el adaptador de RecyclerView para mostrar los elementos de la lista de anime
 class AnimeViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    // Declaración de las vistas que se utilizarán para mostrar la información de anime
     val textAnimeName: TextView
     val textAnimeSeason: TextView
     val textAnimeGenres: TextView
     val imageAnime: ImageView
+    // Binding generado por ViewBinding
     val binding = AnimeItemBinding.bind(view)
-
+    
+    // Inicialización de las vistas utilizando la instancia de binding generada previamente
     init {
         textAnimeName = binding.textName
         textAnimeSeason = binding.textSeason
         textAnimeGenres = binding.textGenres
         imageAnime = binding.imageAnime
     }
-
+    
+    // Función utilizada para mostrar la información de anime en las vistas correspondientes
     fun render(anime: AnimeListItem, onClickListener: (AnimeListItem) -> Unit) {
         textAnimeName.text = anime.name
         textAnimeSeason.text = anime.season
         textAnimeGenres.text = anime.genres
+        // Carga de la imagen de anime utilizando la biblioteca Picasso
         Picasso.get().load(anime.image_url).into(imageAnime)
+        // Configuración de un listener de clic en el elemento de anime
         itemView.setOnClickListener {
             onClickListener(anime)
         }
